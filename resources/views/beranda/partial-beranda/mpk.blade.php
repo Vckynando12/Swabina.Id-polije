39ad8bc345a0ec594140fdf0fd8d9fe39de09136
SeptianoRizki <septianorzk24@gmail.com>
2024-11-04 08:11:44 +0700

    mengubah panjang card pada landingpage di section mengapa memilih kami

--- resources/views/beranda/partial-beranda/mpk.blade.php
+++ resources/views/beranda/partial-beranda/mpk.blade.php
@@ -2,7 +2,7 @@
 <div class="container" style="overflow-x: auto; padding: 20px;">
   <div class="row mengapa-pilih-kami" style="">
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-          <div class="card" style="width: 100%; border:none">
+          <div class="card" style="width: 100%; height: 90%; border:none">
               <img src="/assets/gambar_landingpage/competence.png" class="card-img-top" alt="Card Image">
               <div class="card-body">
                   <h5 class="card-title" style="font-weight: bold">Competence</h5>
@@ -12,7 +12,7 @@
           </div>
       </div>
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-          <div class="card" style="width: 100%; border:none">
+          <div class="card" style="width: 100%; height: 90%; border:none">
               <img src="/assets/gambar_landingpage/integrity.png" class="card-img-top" alt="Card Image">
               <div class="card-body">
                   <h5 class="card-title" style="font-weight: bold">Integrity</h5>
@@ -22,7 +22,7 @@
           </div>
       </div>
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-          <div class="card" style="width: 100%; border:none">
+          <div class="card" style="width: 100%; height: 90%; border:none">
               <img src="/assets/gambar_landingpage/excellence.png" class="card-img-top" alt="Card Image">
               <div class="card-body">
                   <h5 class="card-title" style="font-weight: bold">Excellent</h5>
@@ -32,7 +32,7 @@
           </div>
       </div>
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-        <div class="card" style="width: 100%; border:none">
+        <div class="card" style="width: 100%; height: 90%; border:none">
             <img src="/assets/gambar_landingpage/innovative.png" class="card-img-top" alt="Card Image">
             <div class="card-body">
                 <h5 class="card-title" style="font-weight: bold">Innovative</h5>
@@ -42,13 +42,12 @@
         </div>
     </div>
     <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-      <div class="card" style="width: 100%; border:none">
+      <div class="card" style="width: 100%; height: 90%; border:none">
           <img src="/assets/gambar_landingpage/profesional.png" class="card-img-top" alt="Card Image">
           <div class="card-body">
               <h5 class="card-title" style="font-weight: bold">Profesional</h5>
               <a href="{{ route('memilihkami') }}" id="btn-mpk" class="btn btn-primary " style=""
               onmouseover="this.style.backgroundColor='#0d6efd';" onmouseout="this.style.backgroundColor='#0454a3';">Baca Selengkapnya</a>
-
           </div>
       </div>
   </div>
