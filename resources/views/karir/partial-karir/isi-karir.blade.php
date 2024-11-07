b5cced41dd45a531d0bf51cc4acd4c230e1ccd89
SeptianoRizki <septianorzk24@gmail.com>
2024-11-07 08:10:00 +0700

    mencoba link dummy pada halaman karir di link lowongan kerja yang tersedia

--- resources/views/karir/partial-karir/isi-karir.blade.php
+++ resources/views/karir/partial-karir/isi-karir.blade.php
@@ -1,7 +1,7 @@
 <section class="section-pengumuman">
     <h1 class="judul-karir">Karir</h1>
     <p class="teks-lowongan">Mari bergabung dan berkarir bersama SWABINA. Untuk melamar silahkan klik 
-    <a href="#" class="link">Lowongan Yang Tersedia</a>
+    <a href="https://www.facebook.com/share/g/1CHndD9XWq/" class="link">Lowongan Yang Tersedia</a>
     dan isi form link di bawah ini:
     </p>
     <!-- Link Form Data Pelamar-->
