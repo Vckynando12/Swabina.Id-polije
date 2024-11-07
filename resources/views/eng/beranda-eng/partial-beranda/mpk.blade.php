40078ae7882f7bf16cfd81cd12a3d29e6f2bd27d
SeptianoRizki <septianorzk24@gmail.com>
2024-11-07 09:27:39 +0700

    menyesuaikan ukuran tinggi card di section mengapa memilih kami pada beranda eng menjadi 90%

--- resources/views/eng/beranda-eng/partial-beranda/mpk.blade.php
+++ resources/views/eng/beranda-eng/partial-beranda/mpk.blade.php
@@ -2,51 +2,51 @@
 <div class="container" style="overflow-x: auto; padding: 20px;">
   <div class="row mengapa-pilih-kami" style="">
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-          <div class="card" style="width: 100%; border:none">
+          <div class="card" style="width: 100%; height: 90%; border:none">
               <img src="/assets/gambar_landingpage/competence.png" class="card-img-top" alt="Card Image">
               <div class="card-body">
                   <h5 class="card-title" style="font-weight: bold">Competence</h5>
-                  <a href="#" id="btn-mpk" class="btn btn-primary" style=""
+                  <a href="{{ route('memilihkamiEng') }}" id="btn-mpk" class="btn btn-primary" style=""
                   onmouseover="this.style.backgroundColor='#0d6efd';" onmouseout="this.style.backgroundColor='#0454a3';">Read More</a>
               </div>
           </div>
       </div>
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-          <div class="card" style="width: 100%; border:none">
+          <div class="card" style="width: 100%; height: 90%; border:none">
               <img src="/assets/gambar_landingpage/integrity.png" class="card-img-top" alt="Card Image">
               <div class="card-body">
                   <h5 class="card-title" style="font-weight: bold">Integrity</h5>
-                  <a href="#" id="btn-mpk" class="btn btn-primary" style=""
+                  <a href="{{ route('memilihkamiEng') }}" id="btn-mpk" class="btn btn-primary" style=""
                   onmouseover="this.style.backgroundColor='#0d6efd';" onmouseout="this.style.backgroundColor='#0454a3';">Read More</a>
               </div>
           </div>
       </div>
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-          <div class="card" style="width: 100%; border:none">
+          <div class="card" style="width: 100%; height: 90%; border:none">
               <img src="/assets/gambar_landingpage/excellence.png" class="card-img-top" alt="Card Image">
               <div class="card-body">
                   <h5 class="card-title" style="font-weight: bold">Excellent</h5>
-                  <a href="#" id="btn-mpk" class="btn btn-primary" style=""
+                  <a href="{{ route('memilihkamiEng') }}" id="btn-mpk" class="btn btn-primary" style=""
                   onmouseover="this.style.backgroundColor='#0d6efd';" onmouseout="this.style.backgroundColor='#0454a3';">Read More</a>
               </div>
           </div>
       </div>
       <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-        <div class="card" style="width: 100%; border:none">
+        <div class="card" style="width: 100%; height: 90%; border:none">
             <img src="/assets/gambar_landingpage/innovative.png" class="card-img-top" alt="Card Image">
             <div class="card-body">
                 <h5 class="card-title" style="font-weight: bold">Innovative</h5>
-                <a href="#" id="btn-mpk" class="btn btn-primary" style=""
+                <a href="{{ route('memilihkamiEng') }}" id="btn-mpk" class="btn btn-primary" style=""
                 onmouseover="this.style.backgroundColor='#0d6efd';" onmouseout="this.style.backgroundColor='#0454a3';">Read More</a>
             </div>
         </div>
     </div>
     <div class="col-md-3 mb-3" style="flex: 0 0 auto; width: 20%;">
-      <div class="card" style="width: 100%; border:none">
+      <div class="card" style="width: 100%; height: 90%; border:none">
           <img src="/assets/gambar_landingpage/profesional.png" class="card-img-top" alt="Card Image">
           <div class="card-body">
               <h5 class="card-title" style="font-weight: bold">Profesional</h5>
-              <a href="#" id="btn-mpk" class="btn btn-primary " style=""
+              <a href="{{ route('memilihkamiEng') }}" id="btn-mpk" class="btn btn-primary " style=""
               onmouseover="this.style.backgroundColor='#0d6efd';" onmouseout="this.style.backgroundColor='#0454a3';">Read More</a>
 
           </div>
